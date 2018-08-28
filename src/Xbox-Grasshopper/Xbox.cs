059269de28d52e3a5a5f042987bb7d4c36b00afa
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2018-08-28 16:50:37 -0400

    Project setup

---
+++ src/Xbox-Grasshopper/Xbox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace XboxGrasshopper
+{
+    public class Xbox : GH_Component
+    {
+        public Xbox()
+          : base("Xbox", "Xbox",
+              "Listen to input from an Xbox game controller.",
+              "XboxGrasshopper", "Xbox")
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+        public override Guid ComponentGuid => new Guid("3106204b-c72a-4d0f-b90a-4192999de5c3");
+        protected override System.Drawing.Bitmap Icon => null;
+
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("in", "in", "", GH_ParamAccess.item);
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("out", "out", "", GH_ParamAccess.item);
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string input = "";
+
+            if (!DA.GetData(0, ref input)) return;
+
+            DA.SetData(0, input);
+        }
+
+
+
+    }
+}
---
+++ src/Xbox-Grasshopper/XboxGrasshopperInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace XboxGrasshopper
+{
+    public class XboxGrasshopper : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "XboxGrasshopper";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("19d283ff-00ea-4eba-8b96-5691f04b96fc");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
